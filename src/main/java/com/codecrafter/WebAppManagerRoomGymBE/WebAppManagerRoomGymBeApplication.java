51e5f0c535f82d56188d0b0e02d9574ae3a51084
QuangHuy014 <huynq201104@gmail.com>
2024-09-16 17:37:25 +0700

    :git_first

---
+++ src/main/java/com/codecrafter/WebAppManagerRoomGymBE/WebAppManagerRoomGymBeApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafter.WebAppManagerRoomGymBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppManagerRoomGymBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppManagerRoomGymBeApplication.class, args);
+	}
+
+}

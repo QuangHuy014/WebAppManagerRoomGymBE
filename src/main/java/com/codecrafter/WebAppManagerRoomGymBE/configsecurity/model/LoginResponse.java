42e0c8947aceb2dd9bbabcd6f6ffbbc94daefa52
Han <dnhan.a7.c3tqcap@gmail.com>
2024-10-08 16:11:48 +0700

    no message

--- src/main/java/com/codecrafter/WebAppManagerRoomGymBE/configsecurity/model/LoginResponse.java
+++ src/main/java/com/codecrafter/WebAppManagerRoomGymBE/configsecurity/model/LoginResponse.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Builder
 public class LoginResponse {
     private final String accessToken;
-     private String status; // Trạng thái của phản hồi
-    private String description; // Mô tả thêm về phản hồi
+     private String status;
+    private String description;
 
 }
